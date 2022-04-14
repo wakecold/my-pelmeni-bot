0c9fd18c0fade57ad95c4026d807f784937f37b9
Полина Краснобаева <polinakrasnobaeva@MacBook-Air-Polina.local>
2022-04-15 01:42:04 +0300

    project structure added

---
+++ cmd/pelmeni/bot.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"github.com/wakecold/my-pelmeni-bot/internal/constants"
+	"github.com/wakecold/my-pelmeni-bot/internal/keyboards"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	order := make(map[string]map[string]int)
+
+
+	// TODO: remove
+	bot.Debug = true
+	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
+	// to make sure Telegram knows we've handled previous values and we don't
+	// need them repeated.
+	updateConfig := tgbotapi.NewUpdate(0)
+
+	// Tell Telegram we should wait up to 30 seconds on each request for an
+	// update. This way we can get information just as quickly as making many
+	// frequent requests without having to send nearly as many.
+	updateConfig.Timeout = 30
+
+	// Start polling Telegram for updates.
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	// Let's go through each update that we're getting from Telegram.
+	for update := range updates {
+
+		// Used to work with what user clicked on a keyboard
+		if update.CallbackQuery != nil {
+			data := update.CallbackQuery.Data
+			from := update.CallbackQuery.From.UserName
+			// bot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data))
+
+			if len(order[from]) == 0 {
+				order[from] = make(map[string]int)
+				order[from][data] = 1
+			} else {
+				order[from][data] += 1
+			}
+
+			bot.Send(
+				tgbotapi.NewMessage(
+					update.CallbackQuery.Message.Chat.ID, fmt.Sprintf(
+						"Thank you for your order @%v", from)))
+		}
+		if update.Message == nil {
+			continue
+		}
+
+		// Create a new MessageConfig. We don't have text yet,
+		// so we leave it empty.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case constants.Print:
+			msg.Text = fmt.Sprintf("Current order is %v", order)
+		case constants.Finish:
+
+			orderResult := "Thank you for your order. Your order is \n"
+			for user, val := range order {
+
+				orderResult += user + " ordered: "
+				for itemId, amount := range val {
+					orderResult += strconv.Itoa(amount) + "x " + constants.Goods[itemId]
+				}
+
+				orderResult += "\n"
+			}
+			msg.Text = orderResult
+		case constants.Create:
+			msg.Text = "SMALL PELMENI KEYBOARD"
+			msg.ReplyMarkup = keyboards.SmallPelmeniKeyboard
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+
+			msg.Text = "BIG PELMENI KEYBOARD"
+			msg.ReplyMarkup = keyboards.BigPelmeniKeyboard
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+
+			msg.Text = "SAUCE KEYBOARD"
+			msg.ReplyMarkup = keyboards.SauceKeyboard
+		case constants.Yura:
+			msg.Text = "pidor"
+		default:
+			msg.Text = "I don't know that command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+
+	}
+
+}
---
+++ internal/keyboards/keyboards.go
@@ -0,0 +1,50 @@
+package keyboards
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+
+var SmallPelmeniKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("бык", "1"),
+		tgbotapi.NewInlineKeyboardButtonData("мясные", "2"),
+		tgbotapi.NewInlineKeyboardButtonData("сырные", "3"),
+		tgbotapi.NewInlineKeyboardButtonData("итальянцы", "4"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("креветка", "5"),
+		tgbotapi.NewInlineKeyboardButtonData("бульба", "6"),
+		tgbotapi.NewInlineKeyboardButtonData("мамин сибиряк", "7"),
+		tgbotapi.NewInlineKeyboardButtonData("классика жанра", "8"),
+	),
+)
+
+var BigPelmeniKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("бык", "11"),
+		tgbotapi.NewInlineKeyboardButtonData("мясные", "12"),
+		tgbotapi.NewInlineKeyboardButtonData("сырные", "13"),
+		tgbotapi.NewInlineKeyboardButtonData("итальянцы", "14"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("креветка", "15"),
+		tgbotapi.NewInlineKeyboardButtonData("бульба", "16"),
+		tgbotapi.NewInlineKeyboardButtonData("мамин сибиряк", "17"),
+		tgbotapi.NewInlineKeyboardButtonData("классика жанра", "18"),
+	),
+)
+
+var SauceKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("сметана", "21"),
+		tgbotapi.NewInlineKeyboardButtonData("чесночный", "22"),
+		tgbotapi.NewInlineKeyboardButtonData("мазик", "23"),
+		tgbotapi.NewInlineKeyboardButtonData("острая сметана", "24"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("сметана с песто", "25"),
+		tgbotapi.NewInlineKeyboardButtonData("сацбели", "26"),
+		tgbotapi.NewInlineKeyboardButtonData("томатный с базиликом", "27"),
+	),
+)
